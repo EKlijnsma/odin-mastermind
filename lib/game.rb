889f005a3684500f0691a64bd842d24b7e961db9
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-26 14:34:10 +0200

    add game class
    game class added for the game logic, changed main to create an instance
    of the game class and run the game

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,46 @@
-# Implement game logic
+require_relative 'clue'
+require_relative 'guess'
+require_relative 'peg_sequence'
+require_relative 'peg'
+require_relative 'pin'
+require_relative 'player'
+require_relative 'secret'
 
-# create secret
-# create player
-# start game and run until 12 guesses are made, or the code is broken.
+class Game
+  @@MAX_ROUNDS = 12
+  @@COLORS = [:red, :light_green, :light_yellow, :blue, :grey, :light_magenta, :light_red, :white]
+
+  def initialize
+    self.win = false
+  end
+
+  def play
+    # Generate secret
+    secret = Secret.new(@@COLORS)
+    
+    # Generate player
+    player = Player.new
+    
+    # Game loop
+    @@MAX_ROUNDS.times do |i|
+      puts "---ROUND #{i}---"
+      guess = player.make_guess(@@COLORS)
+      puts "Your guess: #{guess}"
+      clue = secret.evaluate_guess(guess)
+      if (secret.hidden? == false)
+        self.win = true
+        break
+      end
+      puts "Computer responds: #{clue}"
+    end
+
+    self.announce_end
+  end
+
+  
+  def announce_end
+    win ? "You won!" : "You lost, the secret code was #{secret}"
+  end
+
+  attr_accessor :win
+end 
--- main.rb
+++ main.rb
@@ -1,14 +1,4 @@
-require 'colorize'
-require_relative 'lib/secret'
-require_relative 'lib/guess'
-require_relative 'lib/player'
+require_relative 'lib/game'
 
-MAX_ROUNDS = 12
-COLORS = [:red, :light_green, :light_yellow, :blue, :grey, :light_magenta, :light_red, :white]
-
-# new secret
-secret = Secret.new(COLORS)
-# new player
-player = Player.new
-# play game
-puts secret
+game = Game.new
+game.play
